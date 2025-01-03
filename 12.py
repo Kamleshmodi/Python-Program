3303f0d4c71c56a01f737dd5f61b41ba388974c0
Mr. KAMLESH KUMAR GHANCHI <145127161+Kamleshmodi@users.noreply.github.com>
2025-01-03 21:41:01 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,13 @@
+concatenate in excel 
+
+concatenate function id used to join the string
+ >
+ using concatenate function 
+> 
+example : = CONCATENATE(B8," ",C8," ",D8) 
+ " "  is used to add space in between. 
+ >
+ using '&' operator > 
+Example :- =B8 &" "&C8&" "&D8  
+>
+ concatenate string and numerical :- Example :- "your bill is " & B17
