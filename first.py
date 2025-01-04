e3ef7c817722ddd856d7e67bf99808766bbd9f1a
Mr. KAMLESH KUMAR GHANCHI <145127161+Kamleshmodi@users.noreply.github.com>
2025-01-04 21:45:42 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+name = "kamlesh"
+age = 20
+price = True
+print(name,age,price)
+print("my name = ",name)
+print(type(name))
+print(type(age))
+print(type(price))
---
+++ p1.py
@@ -0,0 +1,11 @@
+print("this is light trafic light sytem ! ")
+print("Enter any light color")
+light = input("light : ")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look on road")
+elif(light == "green"):
+    print("start and go ")
+else:
+    print("your light system broken !")
